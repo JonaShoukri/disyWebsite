6072313e9108630e235b89c849b70f684fbceead
Jonas Shoukri <116384674+JonaShoukri@users.noreply.github.com>
2025-02-18 21:24:36 -0500

    reframe to 40vh

--- .next/static/chunks/app_8bac3a._.js
+++ .next/static/chunks/app_8bac3a._.js
@@ -1260,22 +1260,6 @@ var _s = __turbopack_refresh__.signature();
 function Nav() {
     _s();
     const [selectedTab, setSelectedTab] = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useState"])(null);
-    const [offsetY, setOffsetY] = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useState"])(0);
-    (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useEffect"])({
-        "Nav.useEffect": ()=>{
-            const updateOffset = {
-                "Nav.useEffect.updateOffset": ()=>{
-                    const viewportHeight = window.visualViewport?.height || window.innerHeight;
-                    setOffsetY(viewportHeight * 0.46); // Adjust as needed
-                }
-            }["Nav.useEffect.updateOffset"];
-            updateOffset(); // Run initially
-            window.addEventListener("resize", updateOffset);
-            return ({
-                "Nav.useEffect": ()=>window.removeEventListener("resize", updateOffset)
-            })["Nav.useEffect"];
-        }
-    }["Nav.useEffect"], []);
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         children: [
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$framer$2d$motion$2f$dist$2f$es$2f$render$2f$components$2f$motion$2f$proxy$2e$mjs__$5b$app$2d$client$5d$__$28$ecmascript$29$__["motion"].div, {
@@ -1285,7 +1269,7 @@ function Nav() {
                 },
                 animate: selectedTab ? {
                     scale: 0.4,
-                    y: offsetY
+                    y: "40vh"
                 } : {
                     scale: 1,
                     y: 0
@@ -1298,17 +1282,17 @@ function Nav() {
                     className: "flex items-center justify-center min-h-screen scale-50 m:scale-50 lg:scale-100",
                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$app$2f$components$2f$AnimatedLogo$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
                         fileName: "[project]/app/components/Tabs/Nav.tsx",
-                        lineNumber: 35,
+                        lineNumber: 22,
                         columnNumber: 21
                     }, this)
                 }, void 0, false, {
                     fileName: "[project]/app/components/Tabs/Nav.tsx",
-                    lineNumber: 33,
+                    lineNumber: 20,
                     columnNumber: 17
                 }, this)
             }, void 0, false, {
                 fileName: "[project]/app/components/Tabs/Nav.tsx",
-                lineNumber: 28,
+                lineNumber: 15,
                 columnNumber: 13
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
@@ -1316,43 +1300,43 @@ function Nav() {
                     setSelectedTab: setSelectedTab
                 }, void 0, false, {
                     fileName: "[project]/app/components/Tabs/Nav.tsx",
-                    lineNumber: 40,
+                    lineNumber: 27,
                     columnNumber: 17
                 }, this)
             }, void 0, false, {
                 fileName: "[project]/app/components/Tabs/Nav.tsx",
-                lineNumber: 39,
+                lineNumber: 26,
                 columnNumber: 13
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$app$2f$components$2f$Tabs$2f$About$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                 setSelectedTab: setSelectedTab
             }, void 0, false, {
                 fileName: "[project]/app/components/Tabs/Nav.tsx",
-                lineNumber: 43,
+                lineNumber: 30,
                 columnNumber: 13
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$app$2f$components$2f$Tabs$2f$French$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                 setSelectedTab: setSelectedTab
             }, void 0, false, {
                 fileName: "[project]/app/components/Tabs/Nav.tsx",
-                lineNumber: 44,
+                lineNumber: 31,
                 columnNumber: 13
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$app$2f$components$2f$Tabs$2f$Parteners$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                 setSelectedTab: setSelectedTab
             }, void 0, false, {
                 fileName: "[project]/app/components/Tabs/Nav.tsx",
-                lineNumber: 45,
+                lineNumber: 32,
                 columnNumber: 17
             }, this)
         ]
     }, void 0, true, {
         fileName: "[project]/app/components/Tabs/Nav.tsx",
-        lineNumber: 26,
+        lineNumber: 13,
         columnNumber: 9
     }, this);
 }
-_s(Nav, "tW4D4F8QilOTtu4mFidGxDxTA3k=");
+_s(Nav, "p1S/76wvdcHm/kos1DXWkXK/H3c=");
 _c = Nav;
 var _c;
 __turbopack_refresh__.register(_c, "Nav");
--- app/components/Tabs/Nav.tsx
+++ app/components/Tabs/Nav.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {motion} from "framer-motion";
-import {useEffect, useState} from "react";
+import { useState } from "react";
 import Services from './Services'
 import About from './About'
 import French from './French'
@@ -8,27 +8,14 @@ import Partners from "./Parteners";
 import AnimatedLogo from "../AnimatedLogo";
 export default function Nav() {
     const [selectedTab, setSelectedTab] = useState<string | null>(null);
-    const [offsetY, setOffsetY] = useState(0);
-
-    useEffect(() => {
-        const updateOffset = () => {
-            const viewportHeight = window.visualViewport?.height || window.innerHeight;
-            setOffsetY(viewportHeight * 0.46); // Adjust as needed
-        };
-
-        updateOffset(); // Run initially
-
-        window.addEventListener("resize", updateOffset);
-        return () => window.removeEventListener("resize", updateOffset);
-    }, []);
 
     return (
         <div>
 
             <motion.div
-                initial={{ scale: 1, y: 0 }}
-                animate={selectedTab ? { scale: 0.4, y: offsetY } : { scale: 1, y: 0 }}
-                transition={{ duration: 1.2, ease: "easeInOut" }}
+                initial={{scale: 1, y: 0}}
+                animate={selectedTab ? {scale: 0.4, y: "40vh"} : {scale: 1, y: 0}}
+                transition={{duration: 1.2, ease: "easeInOut"}}
             >
                 <div
                     className="flex items-center justify-center min-h-screen scale-50 m:scale-50 lg:scale-100">
