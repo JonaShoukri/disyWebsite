a6f254dfbdaf2dccb6f6421f133af72ad24f14da
Jonas Shoukri <116384674+JonaShoukri@users.noreply.github.com>
2025-03-03 13:50:44 -0500

    another one

--- .next/static/chunks/app_bef181._.js
+++ .next/static/chunks/app_bef181._.js
@@ -390,7 +390,7 @@ function Home() {
                             viewport: {
                                 once: true
                             },
-                            className: "bg-[#CEABC1] w-full absolute bottom-28 md:bottom-0 extra-bold-text text-[#EAEAEC] text-center text-[2vw] p-4",
+                            className: "bg-[#CEABC1] w-full absolute bottom-24 md:bottom-0 extra-bold-text text-[#EAEAEC] text-center text-[2vw] p-4",
                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                                 href: "/services/consulting/appointment",
                                 children: "BOOK A CONSULTATION"
